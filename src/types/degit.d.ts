5714cd724f1ebe9c8df96a49791dfcb233d4faf4
Paolo-Itri <paolo.itri89@gmail.com>
2026-02-20 11:31:31 +0100

    fix: add degit type declaration

---
+++ src/types/degit.d.ts
@@ -0,0 +1,13 @@
+declare module "degit" {
+  interface DegitOptions {
+    cache?: boolean;
+    force?: boolean;
+    verbose?: boolean;
+  }
+
+  interface Emitter {
+    clone(dest: string): Promise<void>;
+  }
+
+  export default function degit(src: string, opts?: DegitOptions): Emitter;
+}
